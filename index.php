c707665380115e60b07ea2f3399c8ec02c9a1074
Pablo Hinojosa <phinojosa@codeko.com>
2025-08-01 09:45:16 +0200

    fix: plugin paths

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: POLA-CDK - Control
  * Description: Currently, only provides a function to check updates for POLA-CDK plugins
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Pola Network
  * Author URI: https://github.com/Codeko/pola-cdk-control
  */
@@ -27,11 +27,11 @@ class PolaCDK_Control {
     public static function self_updater() {
         $config = array(
             'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-            'proper_folder_name' => 'pola-cdk-control', // this is the name of the folder your plugin lives in
-            'api_url' => 'https://api.github.com/repos/Codeko/pola-cdk-control', // the GitHub API url of your GitHub repo
-            'raw_url' => 'https://raw.github.com/Codeko/pola-cdk-control/main', // the GitHub raw url of your GitHub repo
-            'github_url' => 'https://github.com/Codeko/pola-cdk-control', // the GitHub url of your GitHub repo
-            'zip_url' => 'https://github.com/Codeko/pola-cdk-control/zipball/main', // the zip url of the GitHub repo
+            'proper_folder_name' => 'wp-pola-cdk-control', // this is the name of the folder your plugin lives in
+            'api_url' => 'https://api.github.com/repos/Codeko/wp-pola-cdk-control', // the GitHub API url of your GitHub repo
+            'raw_url' => 'https://raw.github.com/Codeko/wp-pola-cdk-control/main', // the GitHub raw url of your GitHub repo
+            'github_url' => 'https://github.com/Codeko/wp-pola-cdk-control', // the GitHub url of your GitHub repo
+            'zip_url' => 'https://github.com/Codeko/wp-pola-cdk-control/zipball/main', // the zip url of the GitHub repo
             'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
             'requires' => '6.4.1', // which version of WordPress does your plugin require?
             'tested' => '6.4.1', // which version of WordPress is your plugin tested up to?
